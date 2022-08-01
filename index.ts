db579e1c1df70b0e7a0e349cd8ec00647a0efffe
Dhairya Desai <dhairya.n.desai@gmail.com>
2022-07-31 21:25:37 -0500

    Set up http server with socket server

--- index.ts
+++ index.ts
@@ -1,15 +1,28 @@
-import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-
+import express, { Express, Request, Response } from 'express';
+import { createServer } from 'http'
+import { Server } from "socket.io";
+// Configure .env file
 dotenv.config();
-
+// Set up express and socket server
 const app: Express = express();
+// Route all requests to default response
+app.get('*', (req: Request, res: Response) => {
+  res.send('Express + Typescript server')
+})
+// Create an http server and pass to socket server
+const httpServer = createServer(app)
+const io = new Server(httpServer, {})
+// On connection event for the socket
+io.on('connection', (socket) => {
+  console.log('Socket connected', socket)
+  // TODO: Handle connections
+  socket.on('disconnect', (reason) => {
+    console.log('Socket disconnected', reason)
+    // TODO: Handle disconnect event
+  })
+})
+// Start HTTP server
+// TODO: use HTTPS in production
 const port = process.env.PORT;
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
-
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
+httpServer.listen(port)
