7212a95cdb1c7a6c40eb0ffcb4759b04d5523b7d
Dhairya Desai <dhairya.n.desai@gmail.com>
2022-07-31 22:35:15 -0500

    Set up cors and enable connections from client.

--- index.ts
+++ index.ts
@@ -2,20 +2,27 @@ import dotenv from 'dotenv';
 import express, { Express, Request, Response } from 'express';
 import { createServer } from 'http'
 import { Server } from "socket.io";
+import cors from 'cors'
 // Configure .env file
 dotenv.config();
 // Set up express and socket server
 const app: Express = express();
+app.use(cors())
 // Route all requests to default response
 app.get('*', (req: Request, res: Response) => {
   res.send('Express + Typescript server')
 })
 // Create an http server and pass to socket server
 const httpServer = createServer(app)
-const io = new Server(httpServer, {})
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST']
+  }
+})
 // On connection event for the socket
 io.on('connection', (socket) => {
-  console.log('Socket connected', socket)
+  console.log('Socket connected', socket.id)
   // TODO: Handle connections
   socket.on('disconnect', (reason) => {
     console.log('Socket disconnected', reason)
@@ -25,4 +32,6 @@ io.on('connection', (socket) => {
 // Start HTTP server
 // TODO: use HTTPS in production
 const port = process.env.PORT;
-httpServer.listen(port)
+httpServer.listen(port, () => {
+  console.log('Server is connected')
+})
